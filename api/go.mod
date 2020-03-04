011801fa622996d2553e678fb7152d686696e5e2
Dave Banerjee <dvbnrg@gmail.com>
2020-03-04 09:33:20 -0600

    data is loaded

--- api/go.mod
+++ api/go.mod
@@ -3,6 +3,9 @@ module github.com/dvbnrg/Golang-Demo
 go 1.14
 
 require (
+	github.com/buger/jsonparser v0.0.0-20191204142016-1a29609e0929 // indirect
+	github.com/go-resty/resty/v2 v2.2.0 // indirect
+	github.com/jinzhu/gorm v1.9.12 // indirect
 	github.com/labstack/echo v3.3.10+incompatible // indirect
 	github.com/labstack/echo/v4 v4.1.15 // indirect
 )
--- api/main.go
+++ api/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/buger/jsonparser"
+	"github.com/go-resty/resty/v2"
 	"github.com/labstack/echo"
 )
 
@@ -11,5 +14,21 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+	e.GET("/load", load)
+	e.Logger.Fatal(e.Start(":80"))
+}
+
+func load(c echo.Context) error {
+	client := resty.New()
+	resp, err := client.R().
+		Get("https://api.code.gov/repos?api_key=IiLkBh9KDgZEhlJ6JghDKjKqWKklAZemuMBfxNow")
+	if err != nil {
+		fmt.Println("Data retrieval error")
+		return err
+	}
+	// fmt.Println("Body	:\n", resp)
+	jsonparser.ArrayEach(data, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
+		fmt.Println(jsonparser.GET())
+	})
+	return nil
 }
